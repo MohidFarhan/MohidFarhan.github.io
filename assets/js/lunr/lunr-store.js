5612197114675beb2ce2ce1cece17f5334e181e3
MohidFarhan <MohidFarhan@users.noreply.github.com>
2025-08-16 01:27:07 +0000

    deploy: 8c79043c0194360ff0b728997f685100cbcc7588

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Limitations of the Schrödinger Equation and the Birth of Particle Physics",
+        "excerpt":"Introduction Welcome to this blog series where the objective is to spread science and make difficult concepts digestible. The content of this blog is for an audience familiar with basic Quantum Mechanics and concepts of matrices. I will show you that with such basic tools, one can track all the...","categories": ["Cosmology","Relativistic Quantum Mechanics","Particle Physics","Relativity"],
+        "tags": [],
+        "url": "/cosmology/relativistic%20quantum%20mechanics/particle%20physics/relativity/limitations-of-schrodinger/",
+        "teaser": null
+      }]
