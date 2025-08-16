c07cef7bf75183857f98e9646302d7cffe190106
MohidFarhan <MohidFarhan@users.noreply.github.com>
2025-08-16 03:04:13 +0000

    deploy: ab041651737f5a25319738cd9e9df5c42a705eb0

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,9 +5,9 @@ var store = [{
         "url": "/cosmology/relativistic%20quantum%20mechanics/particle%20physics/relativity/limitations-of-schrodinger/",
         "teaser": null
       },{
-        "title": "Inside The Atom Where Empty Space Teems With Activity",
-        "excerpt":"title: “Inside the Atom: Where ‘Empty Space’ Teems with Activity” date: 2025-08-16 categories: [Quantum Field Theory, Relativistic Quantum Mechanics, Particle Physics, Atomic and Molecular Physics] use_math: true layout: single author_profile: true read_time: true comments: true \\(\\frac{\\frac{4 \\pi (r_a)^3}{3}}{\\frac{4 \\pi (r_n)^3}{3}}=\\frac{r_a^3}{r_n^3} \\approx (\\frac{10^{-10}}{10^{-15}})^3 m\\approx 10^{15}m\\) This tell us that the atom...","categories": [],
+        "title": "Inside the Atom: Where ‘Empty Space’ Teems with Activity",
+        "excerpt":"Introduction Today we will unravel the fact that in physics, there is no such thing as empty space. What we like to call “empty” is teeming with activity and fluctuations at the quantum scales. This is not just theory, but has measurable consequences, all of which we will get into...","categories": ["Quantum Field Theory","Relativistic Quantum Mechanics","Particle Physics","Atomic and Molecular Physics"],
         "tags": [],
-        "url": "/inside-the-atom-where-empty-space-teems-with-activity/",
+        "url": "/quantum%20field%20theory/relativistic%20quantum%20mechanics/particle%20physics/atomic%20and%20molecular%20physics/inside-the-atom-where-empty-space-teems-with-activity/",
         "teaser": null
       }]
