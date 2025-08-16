6e1e7363e610eff8072fda1a5a63ef9498adbfce
MohidFarhan <MohidFarhan@users.noreply.github.com>
2025-08-16 02:54:22 +0000

    deploy: ad69e086d43597302b8553a44b0fa18e11013477

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Inside The Atom Where Empty Space Teems With Activity",
-        "excerpt":"title: “Inside the Atom: Where ‘Empty Space’ Teems with Activity” date: 2025-08-16 categories: [Quantum Field Theory, Relativistic Quantum Mechanics, Particle Physics, Atomic and Molecular Physics] use_math: true layout: single author_profile: true read_time: true comments: true$ \\(\\frac{\\frac{4 \\pi (r_a)^3}{3}}{\\frac{4 \\pi (r_n)^3}{3}}=\\frac{r_a^3}{r_n^3} \\approx (\\frac{10^{-10}}{10^{-15}})^3 m\\approx 10^{15}m\\) This tell us that the atom...","categories": [],
+        "excerpt":"title: “Inside the Atom: Where ‘Empty Space’ Teems with Activity” date: 2025-08-16 categories: [Quantum Field Theory, Relativistic Quantum Mechanics, Particle Physics, Atomic and Molecular Physics] use_math: true layout: single author_profile: true read_time: true comments: true \\(\\frac{\\frac{4 \\pi (r_a)^3}{3}}{\\frac{4 \\pi (r_n)^3}{3}}=\\frac{r_a^3}{r_n^3} \\approx (\\frac{10^{-10}}{10^{-15}})^3 m\\approx 10^{15}m\\) This tell us that the atom...","categories": [],
         "tags": [],
         "url": "/inside-the-atom-where-empty-space-teems-with-activity/",
         "teaser": null
