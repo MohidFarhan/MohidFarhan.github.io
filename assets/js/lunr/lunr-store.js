ad3d408dd02b6b8b8e9d02630bd4b6f4a489c706
MohidFarhan <MohidFarhan@users.noreply.github.com>
2025-08-17 14:45:54 +0000

    deploy: 24562cff9737497d251dc157d920a65dca60a6d7

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Inside the Atom: Where ‘Empty Space’ Teems with Activity",
-        "excerpt":"Introduction Today we will unravel the fact that in physics, there is no such thing as empty space. What we like to call “empty” is teeming with activity and fluctuations at the quantum scales. This is not just theory, but has measurable consequences, all of which we will get into...","categories": ["Quantum Field Theory","Relativistic Quantum Mechanics","Particle Physics","Atomic and Molecular Physics"],
+        "excerpt":"Introduction Today we will unravel the idea that in physics, there is no such thing as empty space. What we like to call “empty” is teeming with activity and fluctuations at the quantum scales. This is not just theory, but has measurable consequences, all of which we will get into...","categories": ["Quantum Field Theory","Relativistic Quantum Mechanics","Particle Physics","Atomic and Molecular Physics"],
         "tags": [],
         "url": "/quantum%20field%20theory/relativistic%20quantum%20mechanics/particle%20physics/atomic%20and%20molecular%20physics/inside-the-atom-where-empty-space-teems-with-activity/",
         "teaser": null
